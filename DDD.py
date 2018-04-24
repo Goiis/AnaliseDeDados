c809b30d57828ae34b02e52196f588f01a2e669e
Matheus Gois <matheusg_lima@live.com>
2018-04-24 20:10:54 -0300

    Add files via upload

---
+++ DDD.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+24/04
+
+Nome: Gabriela Lopes Tia: 31744591
+Nome: Matheus Gois Tia: 41746491
+Nome: Leonardo Lopes Tia: 31720528
+Nome: Gregory Ramires Tia: 41706692
+"""
+
+#%%
+import random
+ddd = random.sample(range(287),20)
+ddd.sort()
+print(ddd)
